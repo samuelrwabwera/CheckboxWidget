a0a509390d4a1155f9be459030ec69fa31708a32
samuelrwabwera <rwabwerakankunda@gmail.com>
2018-07-23 16:21:00 +0300

    checkboxreferenceset

---
+++ src/components/checkBoxReferenceSetSelectorContainer.ts
@@ -0,0 +1,77 @@
+import { CSSProperties, Component, createElement } from "react";
+
+import "../ui/checkBoxReferenceSetSelector.scss";
+
+interface WrapperProps {
+    class: string;
+    mxObject?: mendix.lib.MxObject;
+    mxform: mxui.lib.form._FormBase;
+    style: string;
+    readOnly: boolean;
+    friendlyId: string;
+}
+
+export interface ContainerProps extends WrapperProps {
+    checkBox: string;
+}
+
+export interface ContainerState {
+    backgroundColor?: string;
+}
+
+export default class CheckBoxReferenceSetSelectorContainer extends Component<ContainerProps, ContainerState > {
+
+    readonly state = {
+        backgroundColor: undefined
+    };
+
+    constructor(props: ContainerProps) {
+        super(props);
+
+        // this.handleChange = this.handleChange.bind(this);
+    }
+
+    render() {
+        // <div class="my-custom-helloworld-widget">
+        //     <input type="color" class="color-picker"> // select a color
+        //     <Input type="text-area" class="content-area"> // apply selected color here
+        // </div>
+
+        return createElement("div",
+            {
+                className: "checkBoxReferenceSetSelector"
+            },
+            createElement("input", { type: "color", className: "color-picker", onChange: this.handleChange.bind(this) }),
+            createElement("textarea", {
+                className: "content-area",
+                style: { backgroundColor: this.state.backgroundColor } as CSSProperties
+            })
+        );
+    }
+
+    private handleChange(event: Event) {
+        this.setState({ backgroundColor: (event.target as HTMLInputElement).value });
+    }
+
+    public static parseStyle(style = ""): {[key: string]: string} {
+        try {
+            return style.split(";").reduce<{[key: string]: string}>((styleObject, line) => {
+                const pair = line.split(":");
+                if (pair.length === 2) {
+                    const name = pair[0].trim().replace(/(-.)/g, match => match[1].toUpperCase());
+                    styleObject[name] = pair[1].trim();
+                }
+                return styleObject;
+            }, {});
+        } catch (error) {
+            CheckBoxReferenceSetSelectorContainer.logError("Failed to parse style", style, error);
+        }
+
+        return {};
+    }
+
+    public static logError(message: string, style?: string, error?: any) {
+        // tslint:disable-next-line:no-console
+        window.logger ? window.logger.error(message) : console.log(message, style, error);
+    }
+}
